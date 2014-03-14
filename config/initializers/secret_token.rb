2ae480098b20e3e7c09d88b18713531aaa45ea28
Steven Sanborn <steven@feedtank.com>
2014-03-14 11:25:16 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cockify::Application.config.secret_key_base = '23c82e968b1bef6e4bcd9239327e27efaed9d82ce548cad1a13d9a1659bc94b4834102e9ebc7e1b350a164c79a968b6e8ad61fe3d0740c0f69a2988932a83475'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cockify::Application.config.session_store :cookie_store, key: '_Cockify_session'
